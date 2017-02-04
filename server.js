a83663027b1620f77ba03e88ebd83e8c5a9663b6
parasjain-12 <jainparas249@gmail.com>
2017-02-04 13:35:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article', function(req, res){
-    res.send('article one is server here');
+    res.sendFile(path.join(__dirname, 'ui', 'artile-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
